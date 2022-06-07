5abed17e7d66e8cbb1b0709f17642417958d4dcc
EJRRSB <elton.romero@rsb-consulting.com>
2022-06-07 17:01:23 +0800

    dashboard.js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,8 +98,7 @@
     </div>
 
 </div>
-@endsection
- 
+@endsection 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
